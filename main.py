2fa820449daa448f60af192deadd1abd679946a7
parhampz1382zz <parham.pazoki.pppz@gmail.com>
2026-02-03 10:21:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,13 @@ import asyncio
 import os
 from aiogram import Bot
 
-TELE_TOKEN = os.getenv("8583286853:AAHl1Llj1i991t81RgyN22FuHplKJk8q61k")
+TOKEN = os.environ.get("TELE_TOKEN")
 CHAT_ID = "@dasteawal"
 
-async def send_message():
-    bot = Bot(token=TELE_TOKEN)
-    try:
-        await bot.send_message(CHAT_ID, "سلام! بات داره کار میکنه 🎉")
-        print("✅ پیام ارسال شد!")
-    except Exception as e:
-        print(f"❌ خطا: {e}")
-    finally:
-        await bot.session.close()
+async def main():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(CHAT_ID, "سلام! بات داره کار میکنه 🎉")
+    await bot.session.close()
+    print("Done!")
 
-if __name__ == "__main__":
-    asyncio.run(send_message())
+asyncio.run(main())
